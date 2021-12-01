affbafc2cf289c46c8e23a51ee1046be824a8dd6
Justin Chadwell <me@jedevc.com>
2021-12-01 07:53:35 +0000

    Initial commit, and add day 1 solution

---
+++ src/bin/day1.rs
@@ -0,0 +1,68 @@
+use advent_of_code_2021::solver::{solve_file, Solver};
+
+struct Day1Solver {
+    scan: Vec<u64>,
+}
+
+impl Solver<u64> for Day1Solver {
+    fn new(problem: &str) -> Self {
+        Day1Solver {
+            scan: problem
+                .split('\n')
+                .map(|line| line.parse().unwrap())
+                .collect(),
+        }
+    }
+
+    fn solve1(&self) -> Option<u64> {
+        let result = Day1Solver::count_increasing(&self.scan);
+        Some(result as u64)
+    }
+
+    fn solve2(&self) -> Option<u64> {
+        let count = 3;
+        let windows = (0..self.scan.len() - count + 1).map(|i| &self.scan[i..i + count]);
+        let windows_sum: Vec<u64> = windows
+            .map(|window| window.iter().copied().reduce(|a, b| a + b).unwrap())
+            .collect();
+        let result = Day1Solver::count_increasing(&windows_sum);
+        Some(result as u64)
+    }
+}
+
+impl Day1Solver {
+    fn count_increasing(items: &[u64]) -> usize {
+        items
+            .iter()
+            .zip(items[1..].iter())
+            .filter(|(first, second)| second > first)
+            .count()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static EXAMPLE: [u64; 10] = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+
+    #[test]
+    fn test_example_part1() {
+        let solver = Day1Solver {
+            scan: EXAMPLE.to_vec(),
+        };
+        assert_eq!(solver.solve1(), Some(7));
+    }
+
+    #[test]
+    fn test_example_part2() {
+        let solver = Day1Solver {
+            scan: EXAMPLE.to_vec(),
+        };
+        assert_eq!(solver.solve2(), Some(5));
+    }
+}
+
+fn main() {
+    solve_file::<u64, Day1Solver>("day1.txt");
+}
---
+++ src/solver.rs
@@ -0,0 +1,26 @@
+use std::fmt::Display;
+use std::fs;
+use std::path::Path;
+
+pub trait Solver<T> {
+    fn new(problem: &str) -> Self;
+    fn solve1(&self) -> Option<T>;
+    fn solve2(&self) -> Option<T>;
+}
+
+pub fn solve<T: Display, S: Solver<T>>(problem: &str) {
+    let solver = S::new(problem);
+    if let Some(result1) = solver.solve1() {
+        println!("part 1: {}", result1);
+    }
+    if let Some(result2) = solver.solve2() {
+        println!("part 2: {}", result2);
+    }
+}
+
+pub fn solve_file<T: Display, S: Solver<T>>(filename: &str) {
+    let filename = Path::new("resources").join(filename);
+    let problem = fs::read_to_string(filename).unwrap();
+    let problem = problem.trim();
+    solve::<T, S>(&problem);
+}
